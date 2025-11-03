866ad18be633236d92c4efd5ca1bef6241f8e0cb
mksigrest <mksigrest@gmail.com>
2025-11-03 14:46:13 -0500

    bug fixes

--- server/app.js
+++ server/app.js
@@ -8,9 +8,9 @@ const helmet = require('helmet');
 
 const router = require('./router.js');
 
-const port = process.env.PORT || proves.env.NODE_PORT || 3000;
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const dbURI = process.env.MONGODB_URI || 'mongod://localhost/DomoMaker';
+const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/DomoMaker';
 mongoose.connect(dbURI).catch((err) => {
     if (err) {
         console.log('Could not connect to database');
