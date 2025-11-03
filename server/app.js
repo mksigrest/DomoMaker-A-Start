2936959f3d263a546827974660f9782cd7df129b
mksigrest <mksigrest@gmail.com>
2025-11-03 14:35:15 -0500

    finished part 1

--- server/app.js
+++ server/app.js
@@ -5,3 +5,15 @@ const favicon = require('serve-favicon');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 const helmet = require('helmet');
+
+const router = require('./router.js');
+
+const port = process.env.PORT || proves.env.NODE_PORT || 3000;
+
+const dbURI = process.env.MONGODB_URI || 'mongod://localhost/DomoMaker';
+mongoose.connect(dbURI).catch((err) => {
+    if (err) {
+        console.log('Could not connect to database');
+        throw err;
+    }
+});
