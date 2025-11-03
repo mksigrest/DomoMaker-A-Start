f7d9e80ed8b0f514871749621185d13711c6d741
mksigrest <mksigrest@gmail.com>
2025-11-03 14:29:55 -0500

    added init app.js code

---
+++ server/app.js
@@ -0,0 +1,7 @@
+const path = require('path');
+const express = require('express');
+const compression = require('compression');
+const favicon = require('serve-favicon');
+const mongoose = require('mongoose');
+const expressHandlebars = require('express-handlebars');
+const helmet = require('helmet');
