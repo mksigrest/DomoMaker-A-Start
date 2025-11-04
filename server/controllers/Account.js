7f662368b893da9b416b6e302dc1ab9ec1b02aae
mksigrest <mksigrest@gmail.com>
2025-11-03 20:07:16 -0500

    updated login function

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -11,7 +11,18 @@ const logout = (req, res) => {
     return res.redirect('/');
 };
 const login = (req, res) => {
+    const username = `${req.body.username}`;
+    const pass = `${req.body.pass}`;
 
+    if (!username || !pass) {
+        return res.status(400).json({ error: 'All fields are required' });
+    }
+    return Account.authenticate(username, pass, (err, account) => {
+        if (err || !account) {
+            return res.status(401).json({ error: 'Wrong username or password!' });
+        }
+        return res.json({ redirect: '/maker' });
+    });
 };
 const signup = async (req, res) => {
     const username = `${req.body.username}`;
