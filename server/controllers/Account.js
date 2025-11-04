f025e05910562642fc3c785f30ebd98a0471a904
mksigrest <mksigrest@gmail.com>
2025-11-03 20:00:51 -0500

    added try block

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -24,6 +24,14 @@ const signup = async (req, res) => {
     if (pass !== pass2) {
         return res.status(400).json({ error: 'Passwords do not match!' });
     }
+    try {
+        const hash = await Account.generateHash(pass);
+        const newAccount = new Account({ username, password: hash });
+        await newAccount.save();
+        return res.json({ redirect: '/maker' });
+    } catch (err) {
+
+    }
 };
 
 module.exports = {
