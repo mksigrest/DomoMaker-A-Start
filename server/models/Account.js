b86a5ecfaf1460b097f91662f00f5dca176c7311
mksigrest <mksigrest@gmail.com>
2025-11-03 19:18:33 -0500

    added page renders in Account

--- server/models/Account.js
+++ server/models/Account.js
@@ -6,6 +6,24 @@
 */
 const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
+const models = require('../models');
+const Account = models.Account;
+
+const loginPage = (req, res) => {
+    return res.render('login');
+};
+const signupPage = (req, res) => {
+    return res.render('signup');
+};
+const logout = (req, res) => {
+    return res.redirect('/');
+};
+const login = (req, res) => {
+
+};
+const signup = (req, res) => {
+
+};
 
 /* When generating a password hash, bcrypt (and most other password hash
    functions) use a "salt". The salt is simply extra data that gets hashed
