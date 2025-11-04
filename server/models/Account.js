b1b17c06c2007b381c50d19f1ba3706ad206dff5
mksigrest <mksigrest@gmail.com>
2025-11-03 19:37:58 -0500

    bugfix, put in wrong account.js

--- server/models/Account.js
+++ server/models/Account.js
@@ -6,24 +6,6 @@
 */
 const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
-const models = require('../models');
-const Account = models.Account;
-
-const loginPage = (req, res) => {
-    return res.render('login');
-};
-const signupPage = (req, res) => {
-    return res.render('signup');
-};
-const logout = (req, res) => {
-    return res.redirect('/');
-};
-const login = (req, res) => {
-
-};
-const signup = (req, res) => {
-
-};
 
 /* When generating a password hash, bcrypt (and most other password hash
    functions) use a "salt". The salt is simply extra data that gets hashed
@@ -91,12 +73,4 @@ AccountSchema.statics.authenticate = async (username, password, callback) => {
 };
 
 AccountModel = mongoose.model('Account', AccountSchema);
-
-module.exports = {
-    loginPage,
-    signupPage,
-    login,
-    logout,
-    signup,
-    AccountModel,
-};
+module.exports = AccountModel;
