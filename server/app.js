0a0f74415fcdc0baa3876be0495a656a3e04101c
mksigrest <mksigrest@gmail.com>
2025-11-09 12:06:06 -0500

    ran install dotenv and moved code into redis connect function

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const path = require('path');
 const express = require('express');
 const compression = require('compression');
@@ -21,29 +22,37 @@ mongoose.connect(dbURI).catch((err) => {
     }
 });
 
-const app = express();
+const redisClient = redis.createClient({
+    url: process.env.REDISCLOUD_URL,;
+});
 
-app.use(helmet());
-app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted`)));
-app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
-app.use(compression());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+redisClient.on('error', err => console.log('Redis Client Error', err));
 
-app.use(session({
-    key: 'sessionid',
-    secret: 'Domo Arigato',
-    resave: false,
-    saveUninitialized: false,
-}));
+redisClient.connect().then(() => {
+    const app = express();
 
-app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
-app.set('view engine', 'handlebars');
-app.set('views', `${__dirname}/../views`);
+    app.use(helmet());
+    app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted`)));
+    app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+    app.use(compression());
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
 
-router(app);
+    app.use(session({
+        key: 'sessionid',
+        secret: 'Domo Arigato',
+        resave: false,
+        saveUninitialized: false,
+    }));
 
-app.listen(port, (err) => {
-    if (err) { throw err; }
-    console.log(`Listening on port ${port}`);
+    app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
+    app.set('view engine', 'handlebars');
+    app.set('views', `${__dirname}/../views`);
+
+    router(app);
+
+    app.listen(port, (err) => {
+        if (err) { throw err; }
+        console.log(`Listening on port ${port}`);
+    });
 });
