644d7ed56240ca89f06e4239dbe50f64e30cb9cd
mksigrest <mksigrest@gmail.com>
2025-11-09 12:58:51 -0500

    updated router.js

--- server/router.js
+++ server/router.js
@@ -8,10 +8,10 @@ const router = (app) => {
     app.get('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signupPage);
     app.post('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signup);
 
-    app.get('/logout', mid.requires.login, controllers.Account.logout);
+    app.get('/logout', mid.requiresLogin, controllers.Account.logout);
 
-    app.get('/maker', mid.requires.login, controllers.Domo.makerPage);
-    app.post('/maker', mid.requires.login, controllers.Domo.makeDomo);
+    app.get('/maker', mid.requiresLogin, controllers.Domo.makerPage);
+    app.post('/maker', mid.requiresLogin, controllers.Domo.makeDomo);
 
     app.get('/', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
 };
