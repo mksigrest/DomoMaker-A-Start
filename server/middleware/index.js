a89ec76e539b30a29c45309c85e27fe4f37bb387
mksigrest <mksigrest@gmail.com>
2025-11-09 12:39:20 -0500

    updated index.js middleware

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -11,3 +11,23 @@ const requiresLogout = (req, res, next) => {
     }
     return next();
 }
+
+const requiresSecure = (req, res, next) => {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+        return res.redirect(`https://${req.hostname}${req.url}`);
+    }
+    return next();
+}
+
+const bypassSecure = (req, res, next) => {
+    next();
+}
+
+module.exports.requiresLogin = requiresLogin;
+module.exports.requiresLogout = requiresLogout;
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports.requiresSecure = requiresSecure;
+} else {
+    module.exports.requiresSecure = bypassSecure;
+}
